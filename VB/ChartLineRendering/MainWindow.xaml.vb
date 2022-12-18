70b2cb6e05208e57ba697cb32707867d230d232f
DevExpressExampleBot <fakeEmail>
2022-12-18 23:10:51 +0400

    Source auto update [skip ci]

--- VB/ChartLineRendering/MainWindow.xaml.vb
+++ VB/ChartLineRendering/MainWindow.xaml.vb
@@ -1,36 +1,36 @@
-﻿Imports System
+Imports System
 Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace ChartLineRendering
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private Const count As Integer = 20000
-		Private ReadOnly rnd As New Random()
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Property LineData() As ObservableCollection(Of Point)
-		Public Property SplineData() As ObservableCollection(Of Point)
+        Const count As Integer = 20000
 
-		Public Sub New()
-			InitializeComponent()
-			GenerateData(count)
-			Me.DataContext = Me
-		End Sub
+        Private ReadOnly rnd As Random = New Random()
 
-		Private Sub GenerateData(ByVal count As Integer)
-'INSTANT VB NOTE: The variable lineData was renamed since Visual Basic does not handle local variables named the same as class members well:
-			Dim lineData_Conflict As New ObservableCollection(Of Point)()
-'INSTANT VB NOTE: The variable splineData was renamed since Visual Basic does not handle local variables named the same as class members well:
-			Dim splineData_Conflict As New ObservableCollection(Of Point)()
-			For i As Integer = 0 To count - 1
-				lineData_Conflict.Add(New Point(i, Math.Sin(i \ 20)))
-				If i Mod 25 = 0 Then
-					splineData_Conflict.Add(New Point(i, rnd.Next(10, 20)))
-				End If
-			Next i
-			LineData = lineData_Conflict
-			SplineData = splineData_Conflict
-		End Sub
-	End Class
+        Public Property LineData As ObservableCollection(Of Point)
+
+        Public Property SplineData As ObservableCollection(Of Point)
+
+        Public Sub New()
+            Me.InitializeComponent()
+            GenerateData(count)
+            DataContext = Me
+        End Sub
+
+        Private Sub GenerateData(ByVal count As Integer)
+            Dim lineData As ObservableCollection(Of Point) = New ObservableCollection(Of Point)()
+            Dim splineData As ObservableCollection(Of Point) = New ObservableCollection(Of Point)()
+            For i As Integer = 0 To count - 1
+                lineData.Add(New Point(i, Math.Sin(i \ 20)))
+                If i Mod 25 = 0 Then splineData.Add(New Point(i, rnd.Next(10, 20)))
+            Next
+
+            Me.LineData = lineData
+            Me.SplineData = splineData
+        End Sub
+    End Class
 End Namespace
